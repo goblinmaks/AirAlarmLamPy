d1e9db5b0272744a364c1e1d6c33a95448eb378c
MaksymGO <goblinmaks@gmail.com>
2023-12-11 16:58:01 +0200

    Create new diagram.json

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
-# NeoPixels Rainbow on MicroPython
-# Wokwi Example https://wokwi.com/arduino/projects/305569065545499202
+# Ukraine Air Alarm Light on MicroPython
 
 from machine import Pin, Signal
 from neopixel import NeoPixel
 from time import sleep
+
+test = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 255), (0, 0, 0)]
 
 rainbow = [
   (126 , 1 , 0),(114 , 13 , 0),(102 , 25 , 0),(90 , 37 , 0),(78 , 49 , 0),(66 , 61 , 0),(54 , 73 , 0),(42 , 85 , 0),
@@ -11,14 +12,19 @@ rainbow = [
   (0 , 62 , 65),(0 , 50 , 77),(0 , 38 , 89),(0 , 26 , 101),(0 , 14 , 113),(0 , 2 , 125),(9 , 0 , 118),(21 , 0 , 106),
   (33 , 0 , 94),(45 , 0 , 82),(57 , 0 , 70),(69 , 0 , 58),(81 , 0 , 46),(93 , 0 , 34),(105 , 0 , 22),(117 , 0 , 10)]
 
-buzzer_pin = Pin(12, Pin.OUT)
 
-pixels = NeoPixel(Pin(13), 5)
+buzzer_pin = Pin(13, Pin.OUT)
+
+pixels = NeoPixel(Pin(12), 5)
 while True:
   buzzer_pin.value(1)
   rainbow = rainbow[-1:] + rainbow[:-1]
   for i in range(5):
-    pixels[i] = rainbow[i]
+    pixels[i] = test[i]
   pixels.write()
   sleep(0.1)
   buzzer_pin.value(0)
+
+onStart()
+  
+  
