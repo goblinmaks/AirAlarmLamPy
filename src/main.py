b486a0f9a770a486851f05e7c51182ad5d5e5d42
MaksymGO <goblinmaks@gmail.com>
2024-02-08 22:31:07 +0200

    Add main.py

--- src/main.py
+++ src/main.py
@@ -1,31 +1,146 @@
-# Ukraine Air Alarm Light on MicroPython
-# https://wokwi.com/projects/383942008295640065
+import machine
+import time
+import network
+import binascii
+import uasyncio as asyncio
+import ujson as json
+import ure as re
+import usocket as socket
 
-from machine import Pin, Signal
-from neopixel import NeoPixel
-from time import sleep
-
-test = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 255), (0, 0, 0)]
-
-rainbow = [
-  (126 , 1 , 0),(114 , 13 , 0),(102 , 25 , 0),(90 , 37 , 0),(78 , 49 , 0),(66 , 61 , 0),(54 , 73 , 0),(42 , 85 , 0),
-  (30 , 97 , 0),(18 , 109 , 0),(6 , 121 , 0),(0 , 122 , 5),(0 , 110 , 17),(0 , 98 , 29),(0 , 86 , 41),(0 , 74 , 53),
-  (0 , 62 , 65),(0 , 50 , 77),(0 , 38 , 89),(0 , 26 , 101),(0 , 14 , 113),(0 , 2 , 125),(9 , 0 , 118),(21 , 0 , 106),
-  (33 , 0 , 94),(45 , 0 , 82),(57 , 0 , 70),(69 , 0 , 58),(81 , 0 , 46),(93 , 0 , 34),(105 , 0 , 22),(117 , 0 , 10)]
+#SSID = "Flower.SHome"
+PASSWORD = "FB:JwJN[J0tY"
+SSID = "Flower"
+PASSWORD = "eZH{3f?P)y8Y"
 
 
-buzzer_pin = Pin(13, Pin.OUT)
+# Timeout for WiFi connection in seconds
+WIFI_CONNECT_TIMEOUT = 60
 
-pixels = NeoPixel(Pin(12), 5)
-while True:
-    buzzer_pin.value(1)
-    rainbow = rainbow[-1:] + rainbow[:-1]
-    for i in range(5):
-        pixels[i] = test[i]
-    pixels.write()
-    sleep(0.1)
-    buzzer_pin.value(0)
+# Define the GPIO pin connected to the button and LED
+BUTTON_PIN = 4
+LED_PIN = 16
 
-onStart()
-  
-  
+# Create a WLAN (WiFi) object
+wlan = network.WLAN(network.STA_IF)
+
+button = machine.Pin(BUTTON_PIN, machine.Pin.IN)
+chip_id = binascii.hexlify(machine.unique_id()).decode()
+
+# Create an event
+button_event = asyncio.Event()
+alarm_event = asyncio.Event()
+
+
+def button_callback(pin):
+    print("Button callback!")
+    # Set the event when the button is pressed
+    button_event.set()
+
+# Set up the button as an input with a callback on the falling edge
+button.irq(trigger=machine.Pin.IRQ_RISING, handler=button_callback)
+
+# Function to connect to WiFi
+def initial_wifi_connect(_ssid, _password):
+    try:
+        wlan.active(False)
+        wlan.active(True)
+        wlan.config(txpower=20)
+        print(wlan.scan())
+        wlan.connect(_ssid, _password)
+        start_time = time.time()
+        while not wlan.isconnected():
+            if time.time() - start_time > WIFI_CONNECT_TIMEOUT:
+                wlan.active(False)
+                return False
+            time.sleep(1)
+            print("Connecting to Wi-Fi...")
+        print("IP Address: ", wlan.ifconfig()[0])
+        return True
+    except Exception as e:
+        wlan.active(False)
+        print('Error connecting to WiFi:', str(e))
+
+
+def start_ap_mode():
+    print('Started in AP mode')
+    ap_id = 'AirAlarmLamp_{}'.format(chip_id)
+    ap = network.WLAN(network.AP_IF)
+    ap.active(True)
+    ap.config(essid=ap_id, dhcp_hostname= ap_id)
+    print("Connect to '{}', IP Address: {}".format(ap_id, ap.ifconfig()[0]))
+
+async def handle_request(reader, writer):
+    # Log client request
+    request_line = await reader.readline()
+    print("Client requested:", request_line.decode().strip())
+
+    # HTML content
+    html_content = """<!DOCTYPE html>
+<html>
+<head><title>MicroPython Web Server</title></head>
+<body>
+<h1>Hello, MicroPython!</h1>
+<p>This is a test page.</p>
+</body>
+</html>
+"""
+    # HTTP response
+    response = "HTTP/1.1 200 OK\r\n"
+    response += "Content-Type: text/html\r\n"
+    response += "Content-Length: {}\r\n\r\n".format(len(html_content))
+    response += html_content
+
+    # Send response to the client
+    await writer.awrite(response.encode("utf-8"))
+    await writer.aclose()
+
+async def web_server():
+    print("web_server start")
+    server = await asyncio.start_server(handle_request, "0.0.0.0", 80)
+    print("Server started at 0.0.0.0:80")
+
+async def button_handler():
+    print("button_handler start")
+    while True:
+        # Wait for the button event to be set
+        await button_event.wait()
+        print("Button pressed!")
+        # Clear the event for the next button press
+        button_event.clear()
+
+
+async def request_handler():
+    print("time_handler start")
+    while True:
+        print("processing ....")
+        await asyncio.sleep_ms(10000)
+
+
+async def lamp_service():
+    print("lamp service started")
+    # Start the button handler task
+    asyncio.create_task(button_handler())
+    asyncio.create_task(request_handler())
+    while True:
+        await asyncio.sleep_ms(100) 
+
+
+def main():
+    print("ESP8266 Chip ID:", chip_id)  
+    loop = asyncio.get_event_loop()
+    #loop.create_task(button_handler())
+    
+    asyncio.run(web_server())    
+
+    if initial_wifi_connect(SSID, PASSWORD):
+        # Run the event loop
+        asyncio.run(lamp_service())
+    else:
+        start_ap_mode()
+    
+    #asyncio.run(lamp_service())
+    loop.run_forever()   
+
+if __name__ == "__main__":
+    main()
+
